48ad829624272501843ea717a30822925b10a0dc
nozomi307 <is0616ei@ed.ritsumei.ac.jp>
2024-07-21 19:29:52 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,17 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function submitQuiz() {
     const form = document.getElementById('quizForm');
-    const username = form.username.value.trim(); // ユーザー名の前後の空白を削除
+    const username = form.username.value.trim(); 
     let score = 0;
 
-    // 各質問の選択肢の値を取り出す
     const questions = ['q1', 'q2', 'q3', 'q4', 'q5'];
 
     questions.forEach(question => {
-        // 現在選択されているラジオボタンの値を取得
         const selectedOption = form.querySelector(`input[name="${question}"]:checked`);
         if (selectedOption) {
-            score += parseInt(selectedOption.value); // 選択されたラジオボタンの値をスコアに加算
+            score += parseInt(selectedOption.value); 
         }
     });
 
