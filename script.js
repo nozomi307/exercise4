9d08023390c017004ed68504ee41e11802e8a061
nozomi307 <is0616ei@ed.ritsumei.ac.jp>
2024-07-21 19:03:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("submitBtn").addEventListener("click", submitQuiz);
+});
+
 function submitQuiz() {
     const form = document.getElementById('quizForm');
     const username = form.username.value.trim(); // 名前の前後の空白を削除
     let score = 0;
 
     const questions = ['q1', 'q2', 'q3', 'q4', 'q5'];
-
+    
     questions.forEach(question => {
         const answer = form[question].value;
         score += parseInt(answer);
